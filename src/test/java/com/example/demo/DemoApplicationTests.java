6ca23f94b272b40d05e586b1ce0f38497dea7d0d
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-10-10 13:17:39 +0200

    Update DemoApplicationTests.java   des testes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.controller.AbsenceController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AbsenceController absenceController; // On teste que ce contrôleur est bien injecté par Spring
 
+    @Test
+    void contextLoads() {
+        assertThat(absenceController).isNotNull(); // Vérifie que le contrôleur est bien instancié
+    }
 }
